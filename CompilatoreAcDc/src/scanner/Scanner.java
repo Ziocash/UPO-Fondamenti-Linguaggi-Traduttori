72b6e3025db5ef6ccdafe5d380b18a7d16089885
Ziocash <31541164+Ziocash@users.noreply.github.com>
2021-12-16 01:31:19 +0100

    Fixed Scanner issues
    [·] Fixed float recognition
    [·] Fixed EOF and skip chars
    [+] Added tests for Scanner class

--- CompilatoreAcDc/src/scanner/Scanner.java
+++ CompilatoreAcDc/src/scanner/Scanner.java
@@ -4,18 +4,25 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PushbackReader;
+import java.lang.management.OperatingSystemMXBean;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.lang.model.util.ElementScanner6;
+import javax.xml.catalog.Catalog;
+
+import exception.LexicalException;
 import token.*;
 
 public class Scanner {
 	final char EOF = (char) -1; // int 65535
-	private int row;
+	private int row = 1;
 	private PushbackReader buffer;
 	private String log;
+
+	private Token token = null;
 
 	private List<Character> skipChars; // ' ', '\n', '\t', '\r', EOF
 	private List<Character> letters; // 'a',...'z'
@@ -51,75 +58,92 @@ public class Scanner {
 		};
 	}
 
-	/*
-	 * private Token peekToken() throws IOException { ..... }
-	 */
+	public Token peekToken() throws IOException, LexicalException {
+		if (token == null)
+			token = nextToken();
+		return token;
+	}
 
-	public Token nextToken() throws IOException {
-
-		// nextChar contiene il prossimo carattere dell'input.
-		char nextChar = peekChar();
-
+	public Token nextToken() throws IOException, LexicalException {
 		// Avanza nel buffer leggendo i carattere in skipChars
 		// incrementando riga se leggi '\n'.
 		// Se raggiungi la fine del file ritorna il Token EOF
-
-		while (nextChar != '\n') {
-			if (skipChars.contains(nextChar))
+		while (skipChars.contains(peekChar())) {
+			if (peekChar() == '\n')
+				row++;
+			if (peekChar() == EOF) {
 				readChar();
-
-			if (numbers.contains(nextChar))
-				return scanNumber();
-
-			if (letters.contains(nextChar))
-				return scanId();
-
-			if (nextChar == EOF)
-				return new Token(TokenType.EOF, row);
+				token = new Token(TokenType.EOF, row);
+				return token;
+			}
+			readChar();
 		}
 
-		// Se nextChar e' in numbers
-		// return scanNumber()
-		// che legge sia un intero che un float e ritorna il Token INUM o FNUM
-		// i caratteri che leggete devono essere accumulati in una stringa
-		// che verra' assegnata al campo valore del Token
+		while (peekChar() != '\n') {
 
-		// Se nextChar e' in letters
-		// return scanId()
-		// che legge tutte le lettere minuscole e ritorna un Token ID o
-		// il Token associato Parola Chiave (per generare i Token per le
-		// parole chiave usate l'HaskMap di corrispondenza
+			// Se nextChar e' in numbers
+			// return scanNumber()
+			// che legge sia un intero che un float e ritorna il Token INUM o FNUM
+			// i caratteri che leggete devono essere accumulati in una stringa
+			// che verra' assegnata al campo valore del Token
+			if (numbers.contains(peekChar()) || peekChar() == '.') {
+				token = scanNumber();
+				return token;
+			}
 
-		// Se nextChar e' in operators
-		// ritorna il Token associato con l'operatore o il delimitatore
+			// Se nextChar e' in operators
+			// ritorna il Token associato con l'operatore o il delimitatore
+			if (letters.contains(peekChar())) {
+				token = scanId();
+				return token;
+			}
 
+			if (operatorsMap.containsKey(peekChar())) {
+				token = new Token(operatorsMap.get(peekChar()), row);
+				return token;
+			}
+			readChar();
+		}
 		// Altrimenti il carattere NON E' UN CARATTERE LEGALE
+		throw new LexicalException("Illegal character in row " + row);
+	}
 
-		return null;
+	private Token scanNumber() throws IOException, LexicalException {
+		StringBuilder result = new StringBuilder();
+		while (numbers.contains(peekChar())) {
+			result.append(readChar());
+		}
+		if (peekChar() != '.')
+			return new Token(TokenType.INT, row, result.toString());
+
+		int count = 0;
+		result.append(readChar());
+		while (numbers.contains(peekChar())) {
+			result.append(readChar());
+			count++;
+		}
+
+		if (count >= 1 && count <= 5)
+			return new Token(TokenType.FLOAT, row, result.toString());
+
+		throw new LexicalException("Uncorrect decimals in row " + row);
 
 	}
 
-	private Token scanNumber() throws IOException {
-		char nextChar = peekChar();
+	// Se nextChar e' in letters
+	// return scanId()
+	// che legge tutte le lettere minuscole e ritorna un Token ID o
+	// il Token associato Parola Chiave (per generare i Token per le
+	// parole chiave usate l'HashMap di corrispondenza
+	private Token scanId() throws IOException, LexicalException {
 		StringBuilder sb = new StringBuilder();
-		Token numberToken = new Token(TokenType.INT, row);
-		while (numbers.contains(nextChar)) {
-			if (nextChar == '.')
-				numberToken.setType(TokenType.FLOAT);
+		while (letters.contains(peekChar())) {
 			sb.append(readChar());
 		}
-		return numberToken;
 
-	}
-
-	private Token scanId() throws IOException {
-		char nextChar = peekChar();
-		StringBuilder sb = new StringBuilder();
-		Token idToken = new Token(TokenType.ID, row);
-		while (letters.contains(nextChar)) {
-			sb.append(readChar());
-		}
-		return null;
+		if (keyWordsMap.containsKey(sb.toString()))
+			return new Token(keyWordsMap.get(sb.toString()), row);
+		return new Token(TokenType.ID, row);
 	}
 
 	private char readChar() throws IOException {
--- CompilatoreAcDc/src/test/TestScanner.java
+++ CompilatoreAcDc/src/test/TestScanner.java
@@ -1,31 +1,111 @@
 package test;
 
+import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.junit.Test;
 
+import exception.LexicalException;
 import scanner.Scanner;
 import token.Token;
 import token.TokenType;
 
 public class TestScanner {
 
-	// @Test
-	// public void testScanEOF() throws IOException {
-	// 	String path = "C:\\Users\\Simone Gattini\\source\\repos\\UPO-Fondamenti-Linguaggi-Traduttori\\CompilatoreAcDc\\src\\test\\data\\testEOF.txt";
-	// 	Scanner scanner = new Scanner(path);
-	// 	assertNull(scanner.nextToken());
-	// 	fail("Unexpected value");
-
-	// }
+	@Test
+	public void testScanId() throws IOException, LexicalException {
+		String path = "C:\\Users\\Simone Gattini\\source\\repos\\UPO-Fondamenti-Linguaggi-Traduttori\\CompilatoreAcDc\\src\\test\\data\\testIdKw.txt";
+		Scanner scanner = new Scanner(path);
+		Token token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.TYINT);
+		assertEquals(token.getRow(), 1);
+		token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.TYFLOAT);
+		assertEquals(token.getRow(), 2);
+		token = scanner.nextToken();
+		assertFalse(token.getType() == TokenType.TYFLOAT);
+		assertTrue(token.getType() == TokenType.ID);
+		assertEquals(token.getRow(), 2);
+		token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.PRINT);
+		assertEquals(token.getRow(), 3);
+		token = scanner.nextToken();
+		assertFalse(token.getType() == TokenType.PRINT);
+		assertTrue(token.getType() == TokenType.ID);
+		assertEquals(token.getRow(), 3);
+		token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.ID);
+		assertEquals(token.getRow(), 4);
+		token = scanner.nextToken();
+		assertFalse(token.getType() == TokenType.TYINT);
+		assertTrue(token.getType() == TokenType.ID);
+		assertEquals(token.getRow(), 5);
+		token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.TYINT);
+		assertEquals(token.getRow(), 6);
+		token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.ID);
+		assertEquals(token.getRow(), 6);
+	}
 
 	@Test
-	public void testScanId() throws IOException {
-		String path = "src/test/data/testIdKw.txt";
+	public void testScanNumber() throws IOException, LexicalException {
+		String path = "C:\\Users\\Simone Gattini\\source\\repos\\UPO-Fondamenti-Linguaggi-Traduttori\\CompilatoreAcDc\\src\\test\\data\\testNumbers.txt";
 		Scanner scanner = new Scanner(path);
+		Token token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.INT);
+		assertEquals(token.getValue(), "30000");
+		assertEquals(token.getRow(), 1);
+		assertThrows(LexicalException.class, () -> {
+			scanner.nextToken();
+		});
+		token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.FLOAT);
+		assertEquals(token.getValue(), "13.454");
+		assertEquals(token.getRow(), 4);
+		token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.FLOAT);
+		assertEquals(token.getValue(), "098.895");
+		assertEquals(token.getRow(), 4);
+		token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.INT);
+		assertEquals(token.getValue(), "45668");
+		assertEquals(token.getRow(), 5);
+		assertThrows(LexicalException.class, () -> {
+			scanner.nextToken();
+		});
+		assertThrows(LexicalException.class, () -> {
+			scanner.nextToken();
+		});
+		token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.EOF);
+		assertEquals(token.getRow(), 8);
+	}
+
+	@Test
+	public void testScanEOF() throws IOException, LexicalException {
+		String path = "C:\\Users\\Simone Gattini\\source\\repos\\UPO-Fondamenti-Linguaggi-Traduttori\\CompilatoreAcDc\\src\\test\\data\\testEOF.txt";
+		Scanner scanner = new Scanner(path);
+		Token token = scanner.nextToken();
+		assertTrue(token.getType() == TokenType.EOF);
+		assertEquals(token.getRow(), 3);
+	}
+
+	@Test
+	public void testPeekToken() throws IOException, LexicalException {
+		String path = "C:\\Users\\Simone Gattini\\source\\repos\\UPO-Fondamenti-Linguaggi-Traduttori\\CompilatoreAcDc\\src\\test\\data\\testNumbers.txt";
+		Scanner scanner = new Scanner(path);
+		Token token = scanner.nextToken();
+		assertEquals(token, scanner.peekToken());
+		token = scanner.nextToken();
+		assertEquals(token, scanner.peekToken());
 	}
 
 }
