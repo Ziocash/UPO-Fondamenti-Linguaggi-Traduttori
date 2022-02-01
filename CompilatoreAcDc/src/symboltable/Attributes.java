944cb369b1b69f397b1ec221801ba5f8e08a443f
Ziocash <31541164+Ziocash@users.noreply.github.com>
2022-02-01 03:13:36 +0100

    SymbolTable implementation
    [+] Added SymbolTable implementation
    [+] Added Attributes class implementation
    [+] Added Javadoc

--- CompilatoreAcDc/src/symboltable/Attributes.java
+++ CompilatoreAcDc/src/symboltable/Attributes.java
@@ -2,21 +2,49 @@ package symboltable;
 
 import ast.LangType;
 
+/**
+ * <p>{@link SymbolTable} integration class.</p>
+ * 
+ * <p>Used to describe or enrich attributes to a variable inserted into the HashMap of the symbol table.</p>
+ */
 public class Attributes {
 	
+	/**
+	 * Variable type.
+	 */
 	private LangType type;
 	
+	/**
+	 * Class constructor.
+	 * @param type the variable type.
+	 */
 	public Attributes(LangType type) {
 		this.type = type;
 	}
 
+	/**
+	 * Returns the variable type.
+	 * @return the variable type.
+	 */
 	public LangType getType() {
 		return type;
 	}
 	
+	/**
+	 * Sets the variable type.
+	 * @param type the variable value to be set.
+	 */
 	public void setType(LangType type) {
 		this.type = type;
 	}
 
-
+	/**
+	 * Returns the variable type.
+	 * 
+	 * @return the variable type.
+	 */
+	@Override
+	public String toString() {
+		return type.toString();
+	}
 }
--- CompilatoreAcDc/src/symboltable/SymbolTable.java
+++ CompilatoreAcDc/src/symboltable/SymbolTable.java
@@ -3,6 +3,11 @@ package symboltable;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
+/**
+ *<p>{@link SymbolTable} class.</p>
+ * 
+ * <p>Represents a symboltable with {@link String} and {@link Attributes}.</p>
+ */
 public class SymbolTable {
     private static HashMap<String, Attributes> table;
 
@@ -10,10 +15,20 @@ public class SymbolTable {
 		throw new IllegalStateException("Utility class");
 	}
 
+	/**
+	 * Symbol table initialization.
+	 */
 	public static void init() {
 		table = new HashMap<>();
 	}
 
+	/**
+	 * <p>Tries to insert a value in the map.</p>
+	 * <p>Adds it if it's not present and returns {@code true}, otherwise {@code false}.</p>
+	 * @param id the variable name (or id).
+	 * @param entry the {@link Attributes} value (containing e.g.: variable type).
+	 * @return {@code true} if id is not already in the map, otherwise {@code false}.
+	 */
 	public static boolean enter(String id, Attributes entry) {
 		Attributes value = table.get(id);
 		if (value != null)
@@ -22,20 +37,33 @@ public class SymbolTable {
 		return true;
 	}
 
+	/**
+	 * <p>Searchs for a given id.</p>
+	 * <p>Returns an {@link Attributes} value containing variable info, otherwise {@code null}.</p>
+	 * @param id the id to search for.
+	 * @return an {@link Attributes} value containing variable info, otherwise {@code null}.
+	 */
 	public static Attributes lookup(String id) {
 		return table.get(id);
 	}
 
-	public static String toStr() { // Per output
+	/**
+	 * Returns a string representing the whole symbol table.
+	 * @return a string representing the whole symbol table.
+	 */
+	public static String toStr() {
 		StringBuilder res = new StringBuilder("symbol table\n=============\n");
 
 		for (Entry<String, Attributes> entry : table.entrySet())
-			res.append(entry.getKey()).append("   \t").append(entry.getValue())
-					.append("\n");
+			res.append(String.format("%s   \t%s%n", entry.getKey(), entry.getValue()));
 
 		return res.toString();
 	}
 
+	/**
+	 * Returns the map size.
+	 * @return the map size.
+	 */
 	public static int size() {
 		return (table.size());
 	}
