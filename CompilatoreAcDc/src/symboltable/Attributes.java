6986903e36241967a5bcdd558569f061f9940307
Ziocash <31541164+Ziocash@users.noreply.github.com>
2022-02-12 21:46:06 +0100

    Update Attributes.java
    [+] Implemented missing attributes in Attributes class

--- CompilatoreAcDc/src/symboltable/Attributes.java
+++ CompilatoreAcDc/src/symboltable/Attributes.java
@@ -3,19 +3,48 @@ package symboltable;
 import ast.LangType;
 
 /**
- * <p>{@link SymbolTable} integration class.</p>
+ * <p>
+ * {@link SymbolTable} integration class.
+ * </p>
  * 
- * <p>Used to describe or enrich attributes to a variable inserted into the HashMap of the symbol table.</p>
+ * <p>
+ * Used to describe or enrich attributes to a variable inserted into the HashMap
+ * of the symbol table.
+ * </p>
  */
 public class Attributes {
-	
+
 	/**
 	 * Variable type.
 	 */
 	private LangType type;
-	
+
+	/**
+	 * Register value associated to variable.
+	 */
+	private char register;
+
+	/**
+	 * Returns the register value associated in {@link SymbolTable}.
+	 * 
+	 * @return the register value associated in {@link SymbolTable}.
+	 */
+	public char getRegister() {
+		return register;
+	}
+
+	/**
+	 * Sets the register value associated in {@link SymbolTable}
+	 * 
+	 * @param register the register value
+	 */
+	public void setRegister(char register) {
+		this.register = register;
+	}
+
 	/**
 	 * Class constructor.
+	 * 
 	 * @param type the variable type.
 	 */
 	public Attributes(LangType type) {
@@ -24,14 +53,16 @@ public class Attributes {
 
 	/**
 	 * Returns the variable type.
+	 * 
 	 * @return the variable type.
 	 */
 	public LangType getType() {
 		return type;
 	}
-	
+
 	/**
 	 * Sets the variable type.
+	 * 
 	 * @param type the variable value to be set.
 	 */
 	public void setType(LangType type) {
