e92b18c9fb1f61957ced04b719bca7f0ba82b1a3
Ziocash <31541164+Ziocash@users.noreply.github.com>
2022-09-16 17:17:49 +0200

    SymbolTable implementation for Lex and CUP
    [+] Added Attributes class for TypeDescriptor result tracking
    [+] Added SymbolTable

---
+++ JCup/src/cup/symboltable/Attributes.java
@@ -0,0 +1,81 @@
+package cup.symboltable;
+
+import ast.TypeDescriptor;
+
+/**
+ * <p>
+ * {@link SymbolTable} integration class.
+ * </p>
+ * 
+ * <p>
+ * Used to describe or enrich attributes to a variable inserted into the HashMap
+ * of the symbol table.
+ * </p>
+ */
+public class Attributes {
+
+	/**
+	 * Variable type.
+	 */
+	private TypeDescriptor type;
+
+	/**
+	 * Register value associated to variable.
+	 */
+	private char register;
+
+	/**
+	 * Returns the register value associated in {@link SymbolTable}.
+	 * 
+	 * @return the register value associated in {@link SymbolTable}.
+	 */
+	public char getRegister() {
+		return register;
+	}
+
+	/**
+	 * Sets the register value associated in {@link SymbolTable}
+	 * 
+	 * @param register the register value
+	 */
+	public void setRegister(char register) {
+		this.register = register;
+	}
+
+	/**
+	 * Class constructor.
+	 * 
+	 * @param type the variable type.
+	 */
+	public Attributes(TypeDescriptor type) {
+		this.type = type;
+	}
+
+	/**
+	 * Returns the variable type.
+	 * 
+	 * @return the variable type.
+	 */
+	public TypeDescriptor getType() {
+		return type;
+	}
+
+	/**
+	 * Sets the variable type.
+	 * 
+	 * @param type the variable value to be set.
+	 */
+	public void setType(TypeDescriptor type) {
+		this.type = type;
+	}
+
+	/**
+	 * Returns the variable type.
+	 * 
+	 * @return the variable type.
+	 */
+	@Override
+	public String toString() {
+		return type.toString();
+	}
+}
---
+++ JCup/src/cup/symboltable/SymbolTable.java
@@ -0,0 +1,70 @@
+package cup.symboltable;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+/**
+ *<p>{@link SymbolTable} class.</p>
+ * 
+ * <p>Represents a symboltable with {@link String} and {@link Attributes}.</p>
+ */
+public class SymbolTable {
+    private static HashMap<String, Attributes> table;
+
+	private SymbolTable() {
+		throw new IllegalStateException("Utility class");
+	}
+
+	/**
+	 * Symbol table initialization.
+	 */
+	public static void init() {
+		table = new HashMap<>();
+	}
+
+	/**
+	 * <p>Tries to insert a value in the map.</p>
+	 * <p>Adds it if it's not present and returns {@code true}, otherwise {@code false}.</p>
+	 * @param id the variable name (or id).
+	 * @param entry the {@link Attributes} value (containing e.g.: variable type).
+	 * @return {@code true} if id is not already in the map, otherwise {@code false}.
+	 */
+	public static boolean enter(String id, Attributes entry) {
+		Attributes value = table.get(id);
+		if (value != null)
+			return false;
+		table.put(id, entry);
+		return true;
+	}
+
+	/**
+	 * <p>Searchs for a given id.</p>
+	 * <p>Returns an {@link Attributes} value containing variable info, otherwise {@code null}.</p>
+	 * @param id the id to search for.
+	 * @return an {@link Attributes} value containing variable info, otherwise {@code null}.
+	 */
+	public static Attributes lookup(String id) {
+		return table.get(id);
+	}
+
+	/**
+	 * Returns a string representing the whole symbol table.
+	 * @return a string representing the whole symbol table.
+	 */
+	public static String toStr() {
+		StringBuilder res = new StringBuilder("symbol table\n=============\n");
+
+		for (Entry<String, Attributes> entry : table.entrySet())
+			res.append(String.format("%s   \t%s%n", entry.getKey(), entry.getValue()));
+
+		return res.toString();
+	}
+
+	/**
+	 * Returns the map size.
+	 * @return the map size.
+	 */
+	public static int size() {
+		return (table.size());
+	}
+}
