2d7b2e79ba9400181250490bec9564b9e2583839
Ziocash <31541164+Ziocash@users.noreply.github.com>
2024-07-19 16:30:07 +0200

    Update TokenType.java
    [+] Added documentation to TokenType enum values

--- CompilatoreAcDc/src/token/TokenType.java
+++ CompilatoreAcDc/src/token/TokenType.java
@@ -1,17 +1,59 @@
 package token;
 
+/**
+ * Representing token types
+ */
 public enum TokenType {
+	/**
+	 * Representing {@code float} keyword
+	 */
 	TYFLOAT,
+	/**
+	 * Representing {@code int} keyword
+	 */
 	TYINT,
+	/**
+	 * Representing {@code print} keyword
+	 */
 	PRINT,
+	/**
+	 * Representing an identifier (variable name)
+	 */
 	ID,
+	/**
+	 * Integer type value
+	 */
 	INT,
+	/**
+	 * Float type (max 5 decimals) value
+	 */
 	FLOAT,
+	/**
+	 * Assign token to match "="
+	 */
 	ASSIGN,
+	/**
+	 * Assign token to match "+"
+	 */
 	PLUS,
+	/**
+	 * Assign token to match "-"
+	 */
 	MINUS,
+	/**
+	 * Assign token to match "*"
+	 */
 	TIMES,
+	/**
+	 * Assign token to match "/"
+	 */
 	DIV,
+	/**
+	 * Assign token to match ";"
+	 */
 	SEMI,
+	/**
+	 * Assign token to match end of file
+	 */
 	EOF;
 }
